759bb6f0350a419c3e9e8947a3904b816ba0be83
Mike Schore <mschore@twitter.com>
2014-01-10 16:42:25 -0800

    Remove whitespace

--- SPDY/SPDYProtocol.h
+++ SPDY/SPDYProtocol.h
@@ -137,7 +137,7 @@ extern NSString *const SPDYOriginUnregisteredNotification;
 @property BOOL enableSettingsMinorVersion;
 
 /**
-  TLS settings for the underlying CFSocketStream. Possible keys and 
+  TLS settings for the underlying CFSocketStream. Possible keys and
   values for TLS settings can be found in CFSocketStream.h
 
   Default is no settings.
